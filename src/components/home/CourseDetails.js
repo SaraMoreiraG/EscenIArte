a83c1968593e7f7b1756f1513b395f644f89c5f9
SaraMoreiraG <sara_moreira@hotmail.es>
2024-02-03 15:10:50 +0100

    adding course-details

--- src/components/home/CourseDetails.js
+++ src/components/home/CourseDetails.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React from "react";
+import "../../images/1.jpg";
 
 const CourseDetails = () => {
   const imagesInfo = [
-    { id: 1, text: 'Descripción de la imagen 1', imgSrc: '/path-to-image-1.jpg' },
-    { id: 2, text: 'Descripción de la imagen 2', imgSrc: '/path-to-image-2.jpg' },
-    { id: 3, text: 'Descripción de la imagen 3', imgSrc: '/path-to-image-3.jpg' },
-    { id: 4, text: 'Descripción de la imagen 4', imgSrc: '/path-to-image-4.jpg' },
+    {
+      id: 1,
+      text: "Descripción de la imagen 1",
+      shortText: "Imagen 1",
+      imgSrc: "https://esceniarte.s3.amazonaws.com/logo.jpeg",
+    },
+    {
+      id: 2,
+      text: "Descripción de la imagen 2",
+      shortText: "Imagen 2",
+      imgSrc: "https://esceniarte.s3.amazonaws.com/logo.jpeg",
+    },
+    {
+      id: 3,
+      text: "Descripción de la imagen 3",
+      shortText: "Imagen 3",
+      imgSrc: "https://esceniarte.s3.amazonaws.com/logo.jpeg",
+    },
+    {
+      id: 4,
+      text: "Descripción de la imagen 4",
+      shortText: "Imagen 4",
+      imgSrc: "https://esceniarte.s3.amazonaws.com/logo.jpeg",
+    },
   ];
 
   return (
     <div className="course-details">
-      <h2>Una gran frase que describa el curso</h2>
+      <div className="header-center">
+        <h2>Una gran frase que describa el curso</h2>
+      </div>
       <div className="images-container">
         {imagesInfo.map((image) => (
-          <div key={image.id} className="image-wrapper">
-            <div className="image-hover">
-              <img src={image.imgSrc} alt={`Descripción ${image.id}`} />
-              <div className="hover-text">
-                <p>{image.text}</p>
+          <div key={image.id} className="details-block">
+            <div className="image-wrapper">
+              <div className="image-hover">
+                <img src={image.imgSrc} alt={`Descripción ${image.id}`} />
+                <div className="hover-text">
+                  <p>{image.text}</p>
+                </div>
               </div>
             </div>
-            <p className="image-description">{image.text}</p>
+            <p className="image-description">{image.shortText}</p>
           </div>
         ))}
       </div>
