32b25b38be12da7cbb9168af39d3edb86d2172b5
SaraMoreiraG <sara_moreira@hotmail.es>
2024-02-29 18:37:30 +0100

    changing router for github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/home";
 import Login from "./views/Login";
 import Dashboard from "./views/Dashboard";
