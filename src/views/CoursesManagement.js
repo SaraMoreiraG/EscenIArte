3b42841f59def5c28d313259832e616bed767a02
SaraMoreiraG <sara_moreira@hotmail.es>
2024-03-01 03:44:29 +0100

    handle module changes

--- src/views/CoursesManagement.js
+++ src/views/CoursesManagement.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function CoursesManagement() {
-  const course = {
+  const mockCourse = {
     id: 1,
     name: "Desbloquea el Potencial Creativo de ChatGPT en las Artes Escénicas",
     modules: [
@@ -88,140 +87,233 @@ function CoursesManagement() {
       },
     ],
   };
-	const selectedModule = 0;
+  const [course, setCourse] = useState(mockCourse);
+  const [selectedModule, setSelectedModule] = useState(0);
+  const [editingModule, setEditingModule] = useState({
+    status: false,
+    moduleIndex: null,
+    moduleName: "",
+    moduleId: null,
+	readyForApiCall: false,
+  });
+  const [addingModule, setAddingModule] = useState(false);
+  const [errorModules, setErrorModules] = useState("");
+
+  useEffect(() => {
+    if (editingModule.readyForApiCall) {
+      setErrorModules("");
+	  console.log(
+        "Llamando a la API con moduleId actualizado:",
+        editingModule.moduleId,
+        editingModule.moduleName
+      );
+      // Aquí iría tu lógica para llamar a la APIrror al llamar a la API:", error));
+    }
+	// eslint-disable-next-line
+  }, [editingModule.readyForApiCall]);
+
+  const handleEditModuleName = (e) => {
+    const newName = e.target.value;
+    setEditingModule((prev) => ({ ...prev, moduleName: newName }));
+  };
+
+  const addNewModule = (id) => {
+	if(editingModule.moduleName !== ''){
+		setEditingModule((prevState) => ({
+			...prevState,
+			moduleId: id,
+			readyForApiCall: true,
+		  }));
+	}
+	else {
+		setErrorModules('Escribe un nombre')
+	}
+  };
+
+  const startEditing = (moduleIndex) => {
+    const currentModule = course.modules[moduleIndex];
+    setEditingModule({
+      status: true,
+      moduleIndex: moduleIndex,
+      moduleName: currentModule.name, // Inicializa con el nombre actual del módulo
+      moduleId: currentModule.id,
+    });
+  };
+
+  const saveModuleChanges = () => {
+    // Verificar si el nombre ha cambiado.
+    const currentModule = course.modules[editingModule.moduleIndex];
+    if (currentModule.name === editingModule.moduleName) {
+      // Si el nombre no ha cambiado, solo salir del modo de edición.
+      console.log("No changes made, exiting edit mode.");
+      setEditingModule({
+        status: false,
+        moduleIndex: null,
+        moduleName: "",
+        moduleId: null,
+      });
+      return; // Finalizar la función aquí.
+    }
+
+    const updatedModules = course.modules.map((module, index) => {
+      if (index === editingModule.moduleIndex) {
+        console.log(
+          "Updating module with id:",
+          module.id,
+          "New name:",
+          editingModule.moduleName
+        );
+        // Aquí iría la lógica para hacer POST a tu API con module.id y editingModule.moduleName.
+        return { ...module, name: editingModule.moduleName };
+      }
+      return module;
+    });
+
+    setCourse({ ...course, modules: updatedModules });
+    setEditingModule({
+      status: false,
+      moduleIndex: null,
+      moduleName: "",
+      moduleId: null,
+    });
+  };
+
+  const deleteModule = (id) => {
+    console.log("delete module id: ", id);
+  };
 
   return (
     <div className="module-details">
-      {course &&
-        (
-          <>
-            <div className="text-center">
-              <h1>
-				{course.name}
-              </h1>
-              <h2>{course.modules[selectedModule].name}</h2>
+      {course && (
+        <>
+          <div className="text-center">
+            <h1>{course.name}</h1>
+            <h2>{course.modules[selectedModule].name}</h2>
+          </div>
+          <div className="row justify-content-center my-5">
+            <div className="col-7">
+              {course.modules[selectedModule].classes.map((clase) => (
+                <div className="class-wrapper row" key={clase.id}>
+                  <div className="col-6 p-0">
+                    <img
+                      src={clase.img.src}
+                      alt={clase.img.alt}
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="col-6 p-3">
+                    <h3>{clase.name}</h3>
+                    <p>{clase.description}</p>
+                    <hr></hr>
+                    <div className="col-6 d-flex justify-content-between my-3">
+                      <span>
+                        <i className="fa-regular fa-clock ms-2"></i>{" "}
+                        {clase.duration}'
+                      </span>
+                      <button className="btn-pdf">
+                        <i className="fa-solid fa-angles-down me-1"></i> PDF
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="row justify-content-center my-5">
-              <div className="col-7">
-                <div className="class-wrapper row">
-                  <div className="col-6 p-0">
-                    <img
-                      src="https://esceniarte-images.s3.amazonaws.com/1-potencial-creativo/modules/2-ChatGPT+y+Creatividad.png"
-                      alt="class 1"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="col-6 p-3">
-                    <h3>Aplicación de DALL·E en las artes escénicas</h3>
-                    <p>
-                      Aprende a transformar tu arte con la visión de DALL·E.
-                    </p>
-                    <hr></hr>
-                    <div className="col-6 d-flex justify-content-between my-3">
-                      <span>
-                        <i className="fa-regular fa-clock ms-2"></i> 7'
-                      </span>
-                      <button className="btn-pdf">
-                        <i className="fa-solid fa-angles-down me-1"></i> PDF
-                      </button>
-                    </div>
+            <div className="col-3">
+              <div className="side-navbar">
+                <div className="d-flex align-items-center justify-content-between p-4">
+                  <h4>Módulos:</h4>
+                  <div>
+                    <i
+                      className="fa-solid fa-circle-plus"
+                      onClick={() => setAddingModule(!addingModule)}
+                    ></i>
                   </div>
                 </div>
-                <div className="class-wrapper row">
-                  <div className="col-6 p-0">
-                    <img
-                      src="https://esceniarte-images.s3.amazonaws.com/1-potencial-creativo/modules/2-ChatGPT+y+Creatividad.png"
-                      alt="class 1"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="col-6 p-3">
-                    <h3>Creación de Buyer Persona con ChatGPT</h3>
-                    <p>
-                      Aprende a diseñar tu audiencia ideal con la inteligencia
-                      de ChatGPT.
-                    </p>
-                    <hr></hr>
-                    <div className="col-6 d-flex justify-content-between my-3">
-                      <span>
-                        <i className="fa-regular fa-clock ms-2"></i> 10'
-                      </span>
-                      <button className="btn-pdf">
-                        <i className="fa-solid fa-angles-down me-1"></i> PDF
-                      </button>
+                <div className="p-4">
+                  {course.modules.map((module, index) => (
+                    <div key={module.id}>
+                      {editingModule.status &&
+                      editingModule.moduleIndex === index ? (
+                        <>
+                          <div className="d-flex mb-3">
+                            <p>{module.id}. </p>
+
+                            <div className="form-group d-flex">
+                              <input
+                                type="text"
+                                className="px-3"
+                                value={editingModule.moduleName || module.name}
+                                onChange={handleEditModuleName}
+                              />
+                            </div>
+                          </div>
+                          <div className="d-flex justify-content-end mb-3">
+                            <i
+                              className="fa-regular fa-circle-check me-3"
+                              onClick={saveModuleChanges}
+                            ></i>
+                            <i
+                              className="fa-regular fa-trash-can"
+                              onClick={() => deleteModule(module.id)}
+                            ></i>
+                          </div>
+                        </>
+                      ) : (
+                        <div>
+                          <p
+                            className={
+                              selectedModule === index
+                                ? "module-title selected m-0"
+                                : "module-title m-0"
+                            }
+                            onClick={() => setSelectedModule(index)}
+                          >
+                            {module.id}. {module.name}
+                          </p>
+                          <div className="d-flex justify-content-end mb-3">
+                            <i
+                              className="fa-regular fa-pen-to-square me-3"
+                              onClick={() => startEditing(index)}
+                            ></i>
+                            <i
+                              className="fa-regular fa-trash-can"
+                              onClick={() => deleteModule(module.id)}
+                            ></i>
+                          </div>
+                        </div>
+                      )}
                     </div>
-                  </div>
-                </div>
-                <div className="class-wrapper row">
-                  <div className="col-6 p-0">
-                    <img
-                      src="https://esceniarte-images.s3.amazonaws.com/1-potencial-creativo/modules/3-IA+en+Produccio%CC%81n+y+Promocio%CC%81n.png"
-                      alt="class 1"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="col-6 p-3">
-                    <h3>Estrategias de Venta y Marketing</h3>
-                    <p>
-                      Impulsa tus ventas y marketing con estrategias innovadoras
-                      gracias a ChatGPT.
-                    </p>
-                    <hr></hr>
-                    <div className="col-6 d-flex justify-content-between my-3">
-                      <span>
-                        <i className="fa-regular fa-clock ms-2"></i> 10'
-                      </span>
-                      <button className="btn-pdf">
-                        <i className="fa-solid fa-angles-down me-1"></i> PDF
-                      </button>
-                    </div>
-                  </div>
+                  ))}
+                  {addingModule && (
+                    <>
+                      <div className="form-group pe-0">
+                        {course.modules.length + 1}.
+                        <input
+                          type="text"
+                          className="ps-3"
+                          placeholder="Módulo"
+                          name="module"
+                          value={editingModule.moduleName}
+                          onChange={handleEditModuleName}
+                        ></input>
+                      </div>
+                      <div className="text-end">
+                        <i
+                          className="fa-regular fa-circle-check"
+                          onClick={() =>
+                            addNewModule(course.modules.length + 1)
+                          }
+                        ></i>
+                      </div>
+                    </>
+                  )}
+                  {errorModules && <p className="text-error">{errorModules}</p>}
                 </div>
               </div>
-              <div className="col-3">
-                <div className="side-navbar">
-                  <h4 className="p-4 ">Módulos:</h4>
-                  <div className="ps-4">
-                    <Link to={"/module/1"}>
-                      <p>
-                        <i className="fa-regular fa-circle me-1"></i> 1.
-                        Introducción a la IA
-                      </p>
-                    </Link>
-                    <Link to={"/module/2"}>
-                      <p>
-                        <i className="fa-solid fa-circle-notch me-1"></i> 2.
-                        Optimización de Comandos y Respuestas
-                      </p>
-                    </Link>
-                    <Link to={"/module/23"}>
-                      <p>
-                        <i className="fa-regular fa-circle me-1"></i> 3.
-                        Estrategias de Venta y Organización
-                      </p>
-                    </Link>
-                    <p>
-                      <i className="fa-solid fa-circle me-1"></i> 4. Innovación
-                      y Generación de Ideas
-                    </p>
-                    <p>
-                      <i className="fa-regular fa-circle me-1"></i> 5. Creación
-                      de Imágenes con DALL·E
-                    </p>
-                    <p>
-                      <i className="fa-regular fa-circle me-1"></i> 6.
-                      Estrategias para Redes Sociales
-                    </p>
-                    <p>
-                      <i className="fa-regular fa-circle me-1"></i> 7. Plugins y
-                      el Potencial de los Modelos GPT
-                    </p>
-                  </div>
-                </div>
-              </div>
             </div>
-          </>
-		)}
+          </div>
+        </>
+      )}
     </div>
   );
 }
